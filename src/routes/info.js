6c490737c4cc02e61e0312247f74a1e962ac721a
Kim-Jaeoh <rlawodh123@hanmail.net>
2023-04-15 20:06:55 +0900

    TEST

--- src/routes/info.js
+++ src/routes/info.js
@@ -1,10 +1,12 @@
 import express from "express";
 import fs from "fs";
 import admin from "firebase-admin";
-import serviceAccount from "../../serviceAccountKey.json" assert { type: "json" };
+// import serviceAccount from "../../serviceAccountKey.json" assert { type: "json" };
 
 const jsonFile = fs.readFileSync("./src/data/data.json", "utf8"); // json 파일 읽기
+const serviceAccountFile = fs.readFileSync("./serviceAccountKey.json", "utf8"); // json 파일 읽기
 const jsonData = JSON.parse(jsonFile);
+const serviceAccount = JSON.parse(serviceAccountFile);
 const router = express.Router();
 
 admin.initializeApp({
