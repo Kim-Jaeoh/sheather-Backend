8979210e249cbf4a625c94666ba3b850bb182e3b
Kim-Jaeoh <rlawodh123@hanmail.net>
2023-05-11 01:31:44 +0900

    FIX: proxy 수정

--- src/app.js
+++ src/app.js
@@ -4,14 +4,19 @@ import bodyParser from "body-parser";
 import feed from "./routes/feed.js";
 import info from "./routes/info.js";
 
+const corsOptions = {
+  origin: "https://sheather.netlify.app",
+  optionsSuccessStatus: 200,
+};
+
 //express 사용
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.json({ limit: "100mb" }));
 app.use(bodyParser.urlencoded({ limit: "100mb", extended: false }));
 
-app.use("/", feed);
-app.use("/", info);
+app.use("/", cors(corsOptions), feed);
+app.use("/", cors(corsOptions), info);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
